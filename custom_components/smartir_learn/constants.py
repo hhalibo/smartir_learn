4742e13e7e56bc4febfe4be853cbd28c3254326e
Seifon <969794240@qq.com>
2025-02-21 11:13:42 +0800

    Update constants.py

--- custom_components/smartir_learn/constants.py
+++ custom_components/smartir_learn/constants.py
@@ -1,10 +1,35 @@
-DOMAIN = "smartir_learn"
-MOCK_DATA = True
+from typing import Final
+
+DOMAIN: Final = "smartir_learn"
+MOCK_DATA: Final = True
+LEARN_TIMEOUT = 60
 
 # 设备类型列表
-DEVICE_TYPES = {
+DEVICE_TYPES: Final = {
     "climate": "Climate",
     "media_player": "Media Player",
     "fan": "Fan",
     "light": "Light"
 }
+
+DEVICE_TEMPLATE = {
+    "climate": {
+        "Template1": "climate1.json",
+        "DEBUG": "climate2.json"
+    },
+    "media_player": {
+        "Template1": "media1.json"
+    },
+    "fan": {
+        "Template1": "fan1.json"
+    },
+    "light": {
+        "Template1": "light1.json"
+    }
+}
+
+# Translation Keys
+TRANSLATION_KEY_DEVICE_IP_MODE: Final = f"component.{DOMAIN}.common.device_ip_mode"
+TRANSLATION_KEY_TEMPLATE_FROM_FILE: Final = f"component.{DOMAIN}.common.template_from_file"
+TRANSLATION_KEY_COMMAND_REPLACE_MAP: Final = f"component.{DOMAIN}.common.command_replace_map"
+TRANSLATION_KEY_DEVICE_TEMPLATE_NAME: Final = f"component.{DOMAIN}.common.device_template_name"
